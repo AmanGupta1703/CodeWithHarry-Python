0459df140060473445f59ece2f437771132df2ee
Kunal Gupta <89504795+AmanGupta1703@users.noreply.github.com>
2023-10-06 18:02:14 +0530

    What is Comments, Escape Sequence and Print ??

---
+++ 03-Comments-Escape-Sequences-Print/main.py
@@ -0,0 +1,27 @@
+
+#? This is a single line comment in Python
+print("Hello, World!") # This line prints the string "Hello, World!"
+
+"""
+The above code snippet is an example of a multi-line comment in Python. It is enclosed 
+within three double quotation marks and can span multiple lines. Multi-line comments 
+are typically used to provide detailed explanations of code or to temporarily disable 
+a block of code.
+"""
+print("Hello, World!")
+
+#? Escape Sequence
+#? The first line of code prints a string that contains a newline character. The \n in the string creates a new line when the string is printed.
+#? The second line of code prints a string that includes a single quote within single quotes. To include a single quote within a single quote string, we use a backslash (\) before the single quote to escape it.
+#? The third line of code prints a string that includes a double quote within double quotes. To include a double quote within a double quote string, we use a backslash (\) before the double quote to escape it.
+#? The fourth line of code prints a string that shows how to represent a new line character as \n.
+print("This is a string with a newline\ncharacter.")
+
+print('\'Add Single Quote\' in Single Quote String.');
+print("\"Add Double Quote\" in Double Quote String.");
+print("New Line: '\\n'");
+
+#? The code is using the print function to display a message on the output screen. The message includes the words "Hello", "World!", and "2023". The sep parameter is set to ", " which means a comma and space will be used to separate the values. The end parameter is set to "!\n" which means an exclamation mark and a new line character will be added at the end of the message.
+print("Hello", "World!", "2023", sep=", ", end="*\n");
+
+print("Author: ", "Aman S. Gupta");
