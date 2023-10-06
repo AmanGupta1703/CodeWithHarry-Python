acb7653cfa7993d53b40c7af4528a5593d433548
Kunal Gupta <89504795+AmanGupta1703@users.noreply.github.com>
2023-10-06 18:00:47 +0530

    Creating First Python Program

---
+++ 02-First-Python-Program/main.py
@@ -0,0 +1,14 @@
+'''
+    - The code above is a simple Python program that prints out a message to the console. The first line of the code uses the `print()` function to output the string "Hello, World" to the console. The `print()` function takes in a string as an argument, and outputs that string to the console. In this case, the string "Hello, World" is passed in as the argument to the `print()` function, and is outputted to the console.
+'''
+
+print("Hello, World");
+
+print("The sun rises in the east,")
+print("Painting the sky with hues of pink and gold.")
+print("Birds chirp and sing,")
+print("Welcoming the new day with joy untold.")
+print("The world awakens,")
+print("Bathed in the warm glow of the sun.")
+print("And as the day unfolds,")
+print("We'll see what adventures it has begun.")
