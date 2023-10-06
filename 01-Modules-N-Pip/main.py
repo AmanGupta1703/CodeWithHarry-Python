902109028fd15e542cab6c0f2c16f1ba50457fa2
Kunal Gupta <89504795+AmanGupta1703@users.noreply.github.com>
2023-10-06 18:00:12 +0530

    What is Modules and Pip??

---
+++ 01-Modules-N-Pip/main.py
@@ -0,0 +1,20 @@
+# Module means code written by someone else that you can use
+
+'''
+- `pip` is a package manager for Python that allows you to easily install and manage external modules. It comes pre-installed with most Python distributions.
+
+- Modules in Python are files that contain Python code and can be imported into other Python files. They can be either internal (built-in) or external (third-party).
+
+- External modules can be installed using `pip`. To install a module, you can use the command `pip install <module_name>` in your terminal or command prompt. Once installed, you can import the module in your Python code using the `import` statement.
+
+- Internal modules are included with Python and can be used without installing any additional software. They can be imported in the same way as external modules.
+'''
+
+# How to import a module (same for external and internal module)
+# import pandas 
+
+# Examples for External Module
+# `pandas`, `numpy`, and `matplotlib`.
+
+# Examples for build - in modules
+# `math`, `random`, and `datetime`
